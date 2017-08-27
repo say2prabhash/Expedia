4e0d6caa920e2230f6011b5b967a9cf0355c9872
Prabhash <Prabhash Mohanty@Dell>
2017-08-27 13:39:00 +0530

    Added functionality of one-way and round-trip switch

--- expedia-assignment.js
+++ expedia-assignment.js
@@ -1,8 +1,30 @@
-$(document).ready(
-  function () {
-    $( "#departure" ).datepicker({
-      changeMonth: true,
-      changeYear: true 
-    });
-}
-);
+	$(document).ready(
+	    function () {
+	       
+	 
+	$("#return").click(function() {
+		$(this).attr("disabled",true);
+	    $("#departure-info-flex").append("<p id=\"returnText\"class=\"text-info\">Returning</p>");
+	    $("#departure-input-box").append("<input id=\"returnDate\" value=\"mm/dd/yyyy\" type=\"text\">");
+	    $('#departure-input-box').find('#datepicker').datepicker();
+	    $(".deparure-info-input-flex").css("width","710px");
+	    $(".departure-info-input-text").css("width","710px");
+	    $('#departure-input-box').css("width","710px")
+	}
+	);
+	 $(".datepicker").datepicker({
+	            changeMonth: true,
+	            changeYear: true
+	        });
+	 
+	 
+	 $("#oneWay").click(function(){
+			$("#returnDate").remove();
+			$("#returnText").remove();
+			$("#return").removeAttr("disabled");
+	}
+	);
+	}
+	 );
+	
+	

d0635707145302213ea71b663c14ea91196b7ea6
Prabhash <pmohanty@tavisca.com>
2017-08-28 13:28:33 +0530

    Solved the switch between one-way and round trip buttons

--- expedia-assignment.js
+++ expedia-assignment.js
@@ -1,16 +1,19 @@
 	$(document).ready(
 	    function () {
-	       
+	    	$("#return-text").hide();
+	    	$("#returning").hide();
+	  $("#button-container").tabs(); 
+	  $("#trip-button-flex").tabs();    
 	$("#return").click(function() {
-		$(this).attr("disabled",true);
-	    $("#departure-info-flex").append("<p id=\"returnText\"class=\"text-info\">Returning</p>");
-	    $("#departure-input-box").append("<input class=\"datepicker\" id=\"returnDate\" value=\"mm/dd/yyyy\" type=\"text\">");
-	    $('#departure-input-box').find('#returnDate').datepicker({changeMonth: true,changeYear: true,minDate:'0m+0d'});
-	    $(".departure-info-input-text").css("width","100%");
-	    $("#deparure-info-flex").css("width","680px");
-	    $('#departure-input-box').css("width","630px");
-	}
-	);
+		$("#return-text").show();
+		$("#returning").show();
+	    // $("#departure-info-flex").append("<p id=\"returnText\"class=\"text-info\">Returning</p>");
+	    // $("#departure-input-box").append("<input class=\"datepicker\" id=\"returnDate\" value=\"mm/dd/yyyy\" type=\"text\">");
+	    // $('#departure-input-box').find('#returnDate').datepicker({changeMonth: true,changeYear: true,minDate:'0m+0d'});
+	    // $(".departure-info-input-text").css("width","100%");
+	    // $("#deparure-info-flex").css("width","680px");
+	    // $('#departure-input-box').css("width","630px");
+	});
 	$("#checkIn").datepicker({
 					changeMonth: true,
 					changeYear: true,
@@ -25,10 +28,11 @@
 	            changeMonth: true,
 	            changeYear: true,
 	            minDate:'0m+0d',
-	            showAnim: "fadeIn"
+	            showAnim: "fadeIn",
+	            dayNamesMin: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
 	        }
 	   );
-	 $("#flight").click(function(){
+	 $("#button-container #flights").click(function(){
 	 		$("#hotel-child-container").css("display","none");
 	 		$("#departure-info-flex").css("width","600px");
 	 		$("#flight-child-container").css("display","block");
@@ -36,17 +40,17 @@
 	 $("#hotels").click(function(){
 	 			$("#flight-child-container").css("display","none");
 	 			$("#hotel-child-container").css("display","block");
+	 			$("#hotel-child-container").find("#button-container").tabs(); 
 	 			$("#departure-info-input-text").css("width","100%");
 	 			$("#departure-input-box").css("width","600px");
 	 }
 	 );
-	 $("#oneWay").click(function(){
-			$("#returnDate").remove();
-			$("#returnText").remove();
-			$("#return").removeAttr("disabled");
+	 $("#oneWay").on("click",function(){
+			$("#return-text").hide();
+	    	$("#returning").hide();
 			$(".departure-info-input-text").css("width","100%");
 			$("#departure-input-box").css("width","628px");
-	}
+			}
 	);
 	}
 	 );
